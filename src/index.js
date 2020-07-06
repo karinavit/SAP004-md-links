36d5bc117c864acbbbbc90961177a4b5ddfd87b0
karinavit <ka.vitangelo@gmail.com>
2020-07-06 17:11:35 -0300

    Inicio da função MD links

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-const linguagens = require('./testando.json')
+const fs = require('fs');
+let myArr = [];
+
+
+
+
+
+//Estão funcionando para teste
+/* const linguagens = require('./testando.json')
 module.exports = { 
 all: linguagens
-}
+} */
 
+
+//Padrão da Laboratória
 /* module.exports = () => {
   // ...
 };
