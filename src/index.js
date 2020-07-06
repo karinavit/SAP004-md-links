311d32189d74712c8eece58d708e1f79406ed3a0
karinavit <ka.vitangelo@gmail.com>
2020-07-06 19:29:15 -0300

    Iniciada a função mdLinksFile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 let myArr = [];
 
+function mdLinksFile(file) {
+  return new Promise((resolve,reject) =>{
+      fs.readFile(file, 'utf8',(err, data) => {
+console.log(data)
+console.log(err)
+      } )
+  })
 
+}
+mdLinksFile('/home/laboratoria/Documentos/SAP004-md-links/README.md')
 
 
 
