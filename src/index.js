7c15529177402445665d9a382cdcd24afd165739
karinavit <ka.vitangelo@gmail.com>
2020-07-07 11:31:49 -0300

    concluída função mdLinksFile, funcionando para leitura de links

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
-const fs = require('fs');
-let myArr = [];
+const fs = require("fs");
+
 
 function mdLinksFile(file) {
-  return new Promise((resolve,reject) =>{
-      fs.readFile(file, 'utf8',(err, data) => {
-console.log(data)
-console.log(err)
-      } )
-  })
-
+  let myArr = [];
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        reject(err.message);
+      } else {
+        const myRegex = /\[([^\[\]]+)\]\((https?:\/\/[^\)]*)\)/gm;
+        const links = data.match(myRegex);
+        links.forEach((element) => {
+           text = element.match(/\[([^\[\]]+)\]/)[1];
+           href = element.match(/\((https?:\/\/[^\)]*)\)/)[1];
+          myArr.push({text,href,file});
+          })
+        resolve(myArr);
+        console.log(myArr)
+        
+      }
+    }); 
+  });
 }
-mdLinksFile('/home/laboratoria/Documentos/SAP004-md-links/README.md')
+mdLinksFile(process.argv[2]);
+//console.log(process.argv[1]);
 
-
-
+//module.exports = mdLinksFile
 //Estão funcionando para teste
 /* const linguagens = require('./testando.json')
 module.exports = { 
 all: linguagens
 } */
-
 
 //Padrão da Laboratória
 /* module.exports = () => {
